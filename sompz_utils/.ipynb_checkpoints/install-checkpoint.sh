c382c08cfe58264c922995095fcbacce70bfadfa
Boyan Yin <by84@duke.edu>
2026-07-01 16:43:37 -0700

    upload sompz data and notbook

---
+++ sompz_utils/.ipynb_checkpoints/install-checkpoint.sh
@@ -0,0 +1,12 @@
+pip install setuptools_scm
+conda install astropy
+conda install matplotlib
+conda install numba
+conda install tqdm
+conda install pyarrow
+conda install camb
+conda install healpy
+conda install seaborn
+pip install git+https://github.com/chto/rail_base.git --no-build-isolation 
+pip install git+https://github.com/chto/tables_io.git
+pip install .
---
+++ sompz_utils/tomobinning_project/README.md
@@ -0,0 +1,2 @@
+Instructions are documented in detail in 
+https://docs.google.com/document/d/1jpgSQE4jZYT8lPR1a0VldOJPnwl7lMPCpbrSOI5O-40/edit?tab=t.0#heading=h.8eysapevw4o7
